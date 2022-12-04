252f8480de657a60581c2ebee01c477a7ecdbc9a
vantuanenvi <tranvantuanen@gmail.com>
2022-12-04 16:40:10 +0700

    home-page-v1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import { useState } from "react";
+// import { useState } from "react";
 import ThemeContextProvider from './context/ThemeContext';
-import SearchBar from './components/SearchBar/SearchBar';
-import FilmList from './components/FilmList/FilmList';
+// import SearchBar from './components/SearchBar/SearchBar';
+// import FilmList from './components/FilmList/FilmList';
 import { Route, Routes } from 'react-router-dom';
-import FilmSeachList from './components/FilmSearchList/FilmSearchList';
-
+// import FilmSeachList from './components/FilmSearchList/FilmSearchList';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
 
-  return (
-    <ThemeContextProvider>
-        <div className="container">
-        </div>
-    </ThemeContextProvider>
+return (
+    <div className="App">
+     <Routes>
+      <Route path='/vn' element = {<Home></Home>}></Route>
+      <Route path='/login' element= {<Login></Login>}></Route>
+     </Routes>
+    </div>
   );
 }
 
